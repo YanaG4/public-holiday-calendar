1d8c2aa0c5ea7ed3cdd355f0029cd9672ec2774c
sinxenon4 <sinxenon4@gmail.com>
2022-09-19 04:00:13 +0300

    Add header

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import './App.scss';
 import Calendar from './Components/Calendar';
+import Header from './Components/Header';
 
 function App() {
   return (
     <div>
+      <Header />
       <Calendar />
     </div>
   );
---
+++ client/src/Components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Header.css'
+
+export default function Header() {
+    return (
+        <div className='header-container'>
+            <div className='header-image-darker'>
+                <div className='text-container'>
+                    <div className='header-text header-main-text'>Public Holiday Calendar</div>
+                    <div className='header-text'>Be aware of the work schedule around the world!</div>
+                </div>
+
+            </div></div>
+    )
+}
