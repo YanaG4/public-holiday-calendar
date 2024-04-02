6a61de037622f79bc0a50ceb7cf29bedf409a18e
YanaG4 <sinxenon4@gmail.com>
2024-04-02 17:57:58 +0300

    Make a separate subscription footer

--- client/src/Components/iCalSubscription/SubscriptionContainer.js
+++ client/src/Components/iCalSubscription/SubscriptionContainer.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Instruction from './Instruction'
 import Subscription from './Subscription'
+import SubscriptionFooter from './SubscriptionFooter'
 
 import './SubscriptionContainer.css'
 
@@ -9,15 +10,7 @@ export default function SubscriptionContainer() {
         <section className='subscription-section'>
             <Subscription />
             <Instruction />
-            <div className='subscription-gradient'>
-                <div className='subscription-decoration'>
-                    <div className="wave-container">
-                        <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
-                            <path d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z" className="shape-fill"></path>
-                        </svg>
-                    </div>
-                </div>
-            </div>
+            <SubscriptionFooter />
         </section>
     )
 }
