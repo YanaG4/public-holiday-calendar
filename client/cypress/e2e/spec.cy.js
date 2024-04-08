b5988931aed8ee54b3cd1a09ee9c43958602aadc
YanaG4 <sinxenon4@gmail.com>
2024-04-08 11:53:53 +0300

    add cypress tests for when the country is selected

--- client/cypress/e2e/spec.cy.js
+++ client/cypress/e2e/spec.cy.js
@@ -1,8 +1,8 @@
-import { LINK_WARNINGS } from "../../src/Components/iCalSubscription/Subscription";
+import { LINK_WARNINGS } from "../../src/constants/values";
 
 describe('Public holiday calendar', () => {
   beforeEach(() => {
-    cy.visit('/')
+    cy.visit('/');
   })
 
   it('Visits the page', () => {
@@ -20,6 +20,35 @@ describe('Public holiday calendar', () => {
     cy.contains('Estonia').click();
     cy.get('[data-testid="chosenCountry"]').should('have.length', 1);
     cy.get('[data-testid="singleLink"]').should('contains', /EE/i);
-    cy.get('[data-testid="multipleLinks"]').should('contains', LINK_WARNINGS.NOT_ENOUGH_COUNTRIES_TEXT);
+    cy.get('[data-testid="multipleLinks"]').should('contain', LINK_WARNINGS.NOT_ENOUGH_COUNTRIES_TEXT);
+  })
+})
+
+describe('Public holiday calendar with chosen country', () => {
+  beforeEach(() => {
+    cy.visit('/');
+    cy.clearLocalStorage()
+    cy.get('[data-testid="searchField"]').click();
+    cy.contains('Estonia').click();
+  })
+
+  it('supports two+ countries and updates links', () => {
+    cy.get('[data-testid="searchField"]').click();
+    cy.contains('France').click();
+    cy.get('[data-testid="chosenCountry"]').should('have.length', 2);
+    cy.get('[data-testid="singleLink"]').should('contains', /EE,FR/i);
+    cy.get('[data-testid="links"]').should('have.length', 2);
+  })
+
+  it('removes country when clicking "Clear" button', () => {
+    cy.get('[data-testid="CancelIcon"]').click();
+    cy.get('[data-testid="chosenCountry"]').should('have.length', 0);
+  })
+
+  it('removes all countries when clicking "Clear all"', () => {
+    cy.get('[data-testid="searchField"]').click();
+    cy.contains('France').click();
+    cy.get('button[title="Clear"]').click();
+    cy.get('[data-testid="chosenCountry"]').should('have.length', 0);
   })
 })
